9399454d71c29d2c018e73a65e03f469155b0ace
multiplex-galaxy <101547813+multiplex-galaxy@users.noreply.github.com>
2023-04-20 12:01:51 +0800

    remove reading api from the example

--- main.js
+++ main.js
@@ -1,18 +1,89 @@
-const axios = require('axios')
+const { getReaderContract, getChainStorage, PreMinedTokenTotalSupply } = require('@mux-network/mux.js')
+const ethers = require('ethers')
 const BigNumber = require('bignumber.js')
 
+const providerConfigs = [
+  { url: 'https://arb1.arbitrum.io/rpc', chainId: 42161 },
+  { url: 'https://api.avax.network/ext/bc/C/rpc', chainId: 43114 },
+  { url: 'https://bsc-dataseed1.binance.org', chainId: 56 },
+  { url: 'https://rpc.ftm.tools/', chainId: 250 },
+  { url: 'https://mainnet.optimism.io', chainId: 10 }
+]
+
 async function main() {
-  const response = await axios.get('https://app.mux.network/api/liquidityAsset')
-  const totalMlp = new BigNumber(response.data['muxLPTotalBalance'])
-  for (let asset of response.data['assets']) {
-    const symbol = asset['symbol']
-    const lpExposure = new BigNumber(asset['lpBalance']).plus(asset['credit']).div(totalMlp)
-    const traderExposure = new BigNumber(asset['shorts']).minus(asset['longs']).div(totalMlp)
+  const { multiChainLiquidity, muxlpTotalSupply } = await getMultiChainLiquidity()
+
+  for (let symbol in multiChainLiquidity) {
+    const lpExposure = multiChainLiquidity[symbol].lpBalance.div(muxlpTotalSupply)
+    const traderExposure = multiChainLiquidity[symbol].totalShortPosition.minus(multiChainLiquidity[symbol].totalLongPosition).div(muxlpTotalSupply)
     console.log(
       'LP exposure:', lpExposure.toFixed(), symbol, 'per MUXLP.',
       'LP+Trader exposure:', lpExposure.plus(traderExposure).toFixed(), symbol, 'per MUXLP'
     )
   }
+}
+
+function getEmptyAsset() {
+  return {
+    isStable: false,
+    lpBalance: new BigNumber(0),
+    totalLongPosition: new BigNumber(0),
+    totalShortPosition: new BigNumber(0),
+  }
+}
+
+async function getMultiChainLiquidity() {
+  const multiChainLiquidity = {}
+
+  let muxlpTotalSupply = new BigNumber(PreMinedTokenTotalSupply)
+  for (const providerConfig of providerConfigs) {
+    const provider = new ethers.providers.StaticJsonRpcProvider(providerConfig.url, providerConfig.chainId)
+    const singleChainLpDeduct = await getSingleChainLiquidity(provider, multiChainLiquidity)
+    muxlpTotalSupply = muxlpTotalSupply.minus(singleChainLpDeduct)
+  }
+
+  for (const symbol in multiChainLiquidity) {
+    if (!multiChainLiquidity[symbol].isStable) {
+      multiChainLiquidity[symbol].lpBalance = multiChainLiquidity[symbol].lpBalance.minus(PreMinedTokenTotalSupply)
+    }
+  }
+
+  return {
+    multiChainLiquidity,
+    muxlpTotalSupply,
+  }
+}
+
+async function getSingleChainLiquidity(provider, multiChainLiquidity) {
+  const reader = await getReaderContract(provider)
+  const state = await getChainStorage(reader)
+
+  for (const asset of state.assets) {
+    if (!(asset.symbol in multiChainLiquidity)) {
+      multiChainLiquidity[asset.symbol] = getEmptyAsset()
+    }
+    const liquidity = multiChainLiquidity[asset.symbol]
+    liquidity.isStable = asset.isStable
+    liquidity.lpBalance = liquidity.lpBalance
+      .plus(asset.spotLiquidity)
+      .minus(asset.collectedFee)
+      .plus(asset.credit)
+    if (!asset.isStable) {
+      liquidity.lpBalance = liquidity.lpBalance.plus(asset.deduct)
+    }
+    liquidity.totalLongPosition = liquidity.totalLongPosition.plus(asset.totalLongPosition)
+    liquidity.totalShortPosition = liquidity.totalShortPosition.plus(asset.totalShortPosition)
+  }
+
+  for (const dex of state.dexes) {
+    dex.assetIds.forEach((assetId, i) => {
+      const symbol = state.assets[assetId].symbol
+      const liquidity = multiChainLiquidity[symbol]
+      liquidity.lpBalance = liquidity.lpBalance.plus(dex.liquidityBalance[i])
+    })
+  }
+
+  return state.lpDeduct
 }
 
 main()
