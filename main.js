2220fef7f3c68ec9a9fe49e178deebcea55a40bd
multiplex-galaxy <101547813+multiplex-galaxy@users.noreply.github.com>
2023-04-20 11:24:12 +0800

    fix: credit is also part of LP exposure

--- main.js
+++ main.js
@@ -5,14 +5,11 @@ async function main() {
   const response = await axios.get('https://app.mux.network/api/liquidityAsset')
   const totalMlp = new BigNumber(response.data['muxLPTotalBalance'])
   for (let asset of response.data['assets']) {
-    if (asset['isStable']) {
-      continue
-    }
     const symbol = asset['symbol']
-    const lpExposure = new BigNumber(asset['lpBalance']).div(totalMlp)
+    const lpExposure = new BigNumber(asset['lpBalance']).plus(asset['credit']).div(totalMlp)
     const traderExposure = new BigNumber(asset['shorts']).minus(asset['longs']).div(totalMlp)
     console.log(
-      'LP exposure:', lpExposure.toFixed(), symbol, 'per MUXLP.', 
+      'LP exposure:', lpExposure.toFixed(), symbol, 'per MUXLP.',
       'LP+Trader exposure:', lpExposure.plus(traderExposure).toFixed(), symbol, 'per MUXLP'
     )
   }
